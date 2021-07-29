c65ec77babef4f5faf090a41c8f565f88a4894b0
liangduoqiang <shs-xxaqbzyldq2@chinaunicom.cn>
2021-07-29 17:02:43 +0800

    init

---
+++ docs/.vitepress/config.js
@@ -0,0 +1,17 @@
+module.exports = {
+    title: '吃灰大全',
+    description: '吃灰大全积累中',
+    // 主题配置
+    themeConfig: {
+        //   头部导航
+        nav: [
+            { text: 'github', link: '/' },
+        ],
+        //   侧边导航
+        sidebar: [
+            { text: 'demo1', link: '/demo1' },
+            { text: 'demo2', link: '/demo2' },
+        ]
+    },
+    base: 'https://liangduoqiang.github.io/learn/'
+}
